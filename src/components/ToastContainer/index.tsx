0fc8d7a82af5e38ecce361a9037a7b8e5ddae712
Oliver <johnoliver021@gmail.com>
2020-07-29 20:51:28 -0300

    adicionando e removendo toasts

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,45 +1,21 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
-import { Container, Toast } from './styles';
 
-const ToastContainer: React.FC = () => {
+import Toast from './Toast';
+
+import { Container } from './styles';
+import { ToastMessage } from '../../hooks/toast';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
+}
+
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
     <Container>
       {/**Info */}
-      <Toast hasDescription>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>aconteceu um erro</strong>
-          <p>Não foi possível fazer login na aplicação</p>
-
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
-      {/**Success */}
-      <Toast type="success" hasDescription={false}>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>aconteceu um erro</strong>
-
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
-      {/**Error */}
-      <Toast type="error" hasDescription>
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>aconteceu um erro</strong>
-          <p>Não foi possível fazer login na aplicação</p>
-
-          <button type="button">
-            <FiXCircle size={18} />
-          </button>
-        </div>
-      </Toast>
+      {messages.map(message => (
+        <Toast key={message.id} message={message}></Toast>
+      ))}
     </Container>
   );
 };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -43,7 +43,11 @@ const SignIn: React.FC = () => {
           const errors = getValidationErrors(err);
           formRef.current?.setErrors(errors);
         }
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
