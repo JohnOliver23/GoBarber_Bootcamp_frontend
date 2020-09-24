10baccf59ac8c949d2ce2578ab322e408396b20e
Jhon Oliver <johnoliver021@gmail.com>
2020-09-24 09:35:35 -0300

    Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { FiPower } from 'react-icons/fi';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+          <Profile>
+            <img
+              src="https://avatars0.githubusercontent.com/u/25158632?s=460&u=5a5c800c99bc7121d6abc52ce3c04ee71061ecfe&v=4"
+              alt="John Oliver"
+            />
+            <div>
+              <span>Bem-vindo</span>
+              <strong> {user.name}</strong>
+            </div>
+          </Profile>
+          <button onClick={signOut} type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
