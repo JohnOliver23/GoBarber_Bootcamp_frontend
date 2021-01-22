e6527c84f15a7290775d687729eb843d08b71d50
Jhon Oliver <johnoliver021@gmail.com>
2021-01-22 12:55:59 -0300

    finalizando testes de autenticação

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -39,7 +39,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
     });
   }, [fieldName, registerField]);
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+    <Container
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+      data-testid="input-container"
+    >
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
